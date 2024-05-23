8f00f38551887f8d85b09f8892efdff6ce8603c7
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-23 20:55:34 +0330

    fixing bugs

--- script.js
+++ script.js
@@ -6,28 +6,42 @@ let index = 0;
 nextBtn.addEventListener("click", nextImage);
 prevBtn.addEventListener("click", prevImage);
 
-function nextImage() {
-  // clearInterval(interval);
-  index++;
-  if (index > 4) {
-    index = 0;
-  }
-  image.style.transform = `translateX(-${100 * index}%)`;
-  interval;
-}
-
-function prevImage() {
-  index--;
-  if (index < 0) {
-    index = 4;
-  }
-  image.style.transform = `translateX(-${100 * index}%)`;
-}
-
-const interval = setInterval(() => {
+let interval = setInterval(() => {
   index++;
   if (index > 4) {
     index = 0;
   }
   image.style.transform = `translateX(-${100 * index}%)`;
 }, 1000);
+
+function nextImage() {
+  clearInterval(interval);
+  index++;
+  if (index > 4) {
+    index = 0;
+  }
+  image.style.transform = `translateX(-${100 * index}%)`;
+  interval = setInterval(() => {
+    index++;
+    if (index > 4) {
+      index = 0;
+    }
+    image.style.transform = `translateX(-${100 * index}%)`;
+  }, 1000);
+}
+
+function prevImage() {
+  clearInterval(interval);
+  index--;
+  if (index < 0) {
+    index = 4;
+  }
+  image.style.transform = `translateX(-${100 * index}%)`;
+  interval = setInterval(() => {
+    index--;
+    if (index < 0) {
+      index = 4;
+    }
+    image.style.transform = `translateX(-${100 * index}%)`;
+  }, 1000);
+}
